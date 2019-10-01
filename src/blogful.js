94f5a277950a7a35ab79260e21dae4374582e10a
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-10-01 15:11:45 -0400

    clean up syntax and missing return for delete in demo of service

--- src/blogful.js
+++ src/blogful.js
@@ -7,4 +7,22 @@ const db = knex({
   client: 'pg'
 });
 
-console.log(ArticlesService.getAllArticles());
+ArticlesService.getAllArticles(db)
+  .then(articles => console.log('get all', articles))
+  .then(() => 
+    ArticlesService.insertArticle(db, {
+      title: 'Finished Product',
+      content: 'Proof of concept this stuff all works',
+      date_published: new Date()
+    })
+  )
+  .then(newArticle => {
+    console.log('new article',newArticle);
+    return ArticlesService
+      .updateArticle(db, newArticle.id, {title: 'updated proof'})
+      .then(() => ArticlesService.getArticleById(db, newArticle.id));
+  })
+  .then(article => {
+    console.log('new and updated',article);
+    return ArticlesService.deleteArticle(db, article.id);
+  });
